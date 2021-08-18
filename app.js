4e3308d3026287243e6d80c9941cb3e81fc907fa
mubashwirphero1 <mubashwir2020@gmail.com>
2021-08-18 12:19:34 +0600

    price calculation uploading

---
+++ app.js
@@ -0,0 +1,35 @@
+function updateCaseQuantity(product, price, isIncrease) {
+    const productInput = document.getElementById(product + '-input');
+    let productNumber = parseInt(productInput.value);
+    if (isIncrease) {
+        productNumber++;
+    }
+    else if (productNumber > 0) {
+        productNumber--;
+    }
+    productInput.value = productNumber;
+
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+}
+
+function getInputId(product) {
+    const productInput = document.getElementById(product + '-input');
+    const productNumber = parseInt(productInput.value);
+
+    return productNumber;
+}
+function calculateTotal() {
+    const phoneTotal = getInputId('phone') * 1219;
+    const caseTotal = getInputId('case') * 59;
+
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalAmount = subTotal + tax;
+
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax').innerText = tax;
+    document.getElementById('total').innerText = totalAmount;
+}
+
+calculateTotal();
